76cc79f352f27af6f6d2aacf27bdfad07fcb4fcd
ABBorges1 <adrielbarborges@gmail.com>
2022-06-16 18:01:38 -0300

    Teste

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     host: 'localhost',
     dialect: 'mysql',
     username: 'root',
-    password: '21111987Ipa',
+    password: '14189512',
     database: 'solar_limp',
     define: {
         timestamps:true,
